f36b0c42082ba083f6b2f2c7dad846e86bdfc0be
Andrew Capp <acapp909@gmail.com>
2019-12-16 22:58:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,7 @@ class Student
   def self.create (attributes)
     attributes[:name]
     student = Student.new(attributes[:name], attributes[:grade])
-    binding.pry
+    #binding.pry
     student.save
     student
   end
